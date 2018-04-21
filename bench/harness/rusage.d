f6ad81167124e35f7d9e775c706aefd97a45a77b
Dmitry Olshansky <dmitry.olsh@gmail.com>
2018-04-21 22:20:42 +0300

    Fix rusage to use deltas for cpu time (previsouly cumulative)

--- bench/harness/rusage.d
+++ bench/harness/rusage.d
@@ -9,14 +9,25 @@ import core.sys.posix.unistd;
 import core.sys.posix.signal;
 
 struct Stats {
-    SysTime dt;
     string name;
-    ulong rbytes, wbytes; // read/write(-ish) syscalls
-    ulong stime, utime; // CPU time in clock ticks
-    ulong rss; // resident memory pages
+    long rbytes, wbytes; // read/write(-ish) syscalls
+    long stime, utime; // CPU time in clock ticks
+    long rss; // resident memory pages
+
+    auto delta(Stats rhs) {
+        return Stats(
+            name,
+            rbytes - rhs.rbytes,
+            wbytes - rhs.wbytes,
+            stime - rhs.stime,
+            utime - rhs.utime,
+            rss
+        );
+    }
 }
 
 shared int targetPid;
+immutable double MB = 1024.0 * 1024.0;
 
 extern(C) void signal_handler(int sig, siginfo_t*, void*)
 {
@@ -24,7 +35,7 @@ extern(C) void signal_handler(int sig, siginfo_t*, void*)
     _exit(9);
 }
 
-Stats statsOf(SysTime dt, int pid) {
+Stats statsOf(int pid) {
     string ioPath = format("/proc/%d/io", pid);
     string statPath = format("/proc/%d/stat", pid);
     long rd;
@@ -36,20 +47,25 @@ Stats statsOf(SysTime dt, int pid) {
     string name, rest;
     stat.readf("%d (%s) %s", _, name, rest);
     string[] fields = rest.split(" ");
-    ulong stime = fields[11].to!ulong;
-    ulong utime = fields[12].to!ulong;
-    ulong rss = fields[21].to!ulong;
-    return Stats(dt, name, rd, wr, stime, utime, rss);
+    long stime = fields[11].to!long;
+    long utime = fields[12].to!long;
+    long rss = fields[21].to!long;
+    return Stats(name, rd, wr, stime, utime, rss);
 }
 
 int main(string[] argv) {
-    if (argv.length < 2) {
-        stderr.writeln("Usage: rusage <command> [args*]");
+    if (argv.length < 3) {
+        stderr.writeln("Usage: rusage period:log-file <command> [args*]\n\n" ~
+            "Example: rusage 0.1:usage.csv find /\nTo sample each 0.1 second and store to usage.csv");
         return 1;
     }
-    double MB = 1024.0 * 1024.0;
-    File log = File("rusage.log", "w");
-    Pid p = spawnProcess(argv[1..$]);
+    string params = argv[1];
+    double period;
+    string logFile;
+    params.formattedRead("%f:%s", period, logFile);
+    File log = File(logFile, "w");
+    Duration sampling = (period*1000).to!int.msecs;
+    Pid p = spawnProcess(argv[2..$]);
     double pageSize = 1.0*sysconf(_SC_PAGE_SIZE);
     double tickSize = 1.0*sysconf(_SC_CLK_TCK);
     targetPid = p.processID;
@@ -57,21 +73,30 @@ int main(string[] argv) {
     action.sa_sigaction = &signal_handler;
     enforce(sigaction(SIGTERM, &action, null) >= 0);
     enforce(sigaction(SIGINT, &action, null) >= 0);
-    writeln("name,read(MB),written(MB),kernel cpu time(sec),user cpu time(sec),RSS(MB)");
+    log.writeln("name,read(MB),written(MB),kernel cpu time(sec),user cpu time(sec),RSS(MB)");
+    Stats prev;
     for(;;) {
         auto status = tryWait(p);
         if (status.terminated) break;
-        SysTime dt = Clock.currTime;
-        auto stats = statsOf(dt, targetPid);
-        writefln("%s,%s,%f,%f,%f,%f,%f", 
-            dt.toISOExtString, stats.name,
-            stats.rbytes / MB,
-            stats.wbytes / MB,
-            stats.stime / tickSize,
-            stats.utime / tickSize, 
-            stats.rss*pageSize / MB
-        );
-        Thread.sleep(200.msecs);
+        try {
+            auto stats = statsOf(targetPid);
+            auto delta =  stats.delta(prev);
+            prev = stats;
+            SysTime dt = Clock.currTime;
+            log.writefln("%s,%s,%f,%f,%f,%f,%f", 
+                dt.toISOExtString, stats.name,
+                delta.rbytes / MB,
+                delta.wbytes / MB,
+                delta.stime / tickSize,
+                delta.utime / tickSize, 
+                delta.rss * pageSize / MB
+            );
+        }
+        catch (Exception e){
+            stderr.writeln("Error: ", e);
+        }
+        log.flush();
+        Thread.sleep(sampling);
     }
     return 0;
 }
