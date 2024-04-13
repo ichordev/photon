cd7dd515d4faaed15d1bf18d1c1d8bc49909e19e
Dmitry Olshansky <dmitry@olshansky.me>
2024-04-13 18:08:36 +0300

    Initial Windows support

--- src/photon/windows/core.d
+++ src/photon/windows/core.d
@@ -3,25 +3,16 @@ version(Windows):
 private:
 
 import core.sys.windows.core;
+import core.sys.windows.winsock2;
 import core.atomic;
-import core.internal.spinlock;
-import core.stdc.stdlib;
 import core.thread;
 import std.exception;
 import std.windows.syserror;
 import std.random;
-import std.format;
+import std.stdio;
+
 import photon.ds.intrusive_queue;
-
-// T becomes thread-local b/c it's stolen from shared resource
-auto steal(T)(ref shared T arg)
-{
-    for (;;) {
-        auto v = atomicLoad(arg);
-        if(cas(&arg, v, cast(shared(T))null)) return v;
-    }
-}
-
+import photon.windows.support;
 
 shared struct RawEvent {
 nothrow:
@@ -36,7 +27,8 @@ nothrow:
     }
     
     void trigger() { 
-        SetEvent(cast(HANDLE)ev);
+        auto ret = SetEvent(cast(HANDLE)ev);
+        assert(ret != 0);
     }
     
     HANDLE ev;
@@ -48,8 +40,6 @@ struct SchedulerBlock {
     size_t[1] padding;
 }
 static assert(SchedulerBlock.sizeof == 64);
-
-package(photon) __gshared SchedulerBlock[] scheds;
 
 class FiberExt : Fiber { 
     FiberExt next;
@@ -74,38 +64,146 @@ class FiberExt : Fiber {
     }
 }
 
-FiberExt currentFiber; 
-shared RawEvent termination; // termination event, triggered once last fiber exits
-shared HANDLE eventLoop; // event loop, runs outside of D runtime
-shared int alive; // count of non-terminated Fibers scheduled
-enum int MAX_EVENTS = 500;
+package(photon) shared SchedulerBlock[] scheds;
 
-public void startloop()
-{
-    import core.cpuid;
-    uint threads = threadsPerCPU;
+FiberExt currentFiber; 
+__gshared RawEvent termination; // termination event, triggered once last fiber exits
+__gshared HANDLE iocp; // IO Completion port
+shared int alive; // count of non-terminated Fibers scheduled
+
+
+public void startloop() {
+    SYSTEM_INFO info;
+    GetSystemInfo(&info);
+    // TODO: handle NUMA case
+    uint threads = info.dwNumberOfProcessors;
     scheds = new SchedulerBlock[threads];
-    foreach (ref sched; scheds)
-        sched = SchedulerBlock();
+    foreach(ref sched; scheds) {
+        sched.queue = IntrusiveQueue!(FiberExt, RawEvent)(RawEvent(0));
+    }
+    termination = RawEvent(false);
+    iocp = CreateIoCompletionPort(cast(HANDLE)INVALID_HANDLE_VALUE, null, 0, 1);
+    wenforce(iocp != null, "Failed to create IO Completion Port");
+    wenforce(CreateThread(null, 0, &eventLoop, null, 0, null) != null, "Failed to start event loop");
 }
 
 
-public void go(void delegate() func)
-{
-    
-    // power of 2 random choices:
-    size_t a = uniform!"[)"(0, scheds.length);
-    size_t b = uniform!"[)"(0, scheds.length);
-    uint loadA = scheds[a].assigned; // take into account active queue.size?
-    uint loadB = scheds[b].assigned; // ditto
-    size_t c = loadA < loadB ? a : b;
-    atomicOp!"+="(scheds[c].assigned, 1);
-   
+public void go(void delegate() func) {
+    import std.random;
+    uint choice;
+    if (scheds.length == 1) choice = 0;
+    else {
+        uint a = uniform!"[)"(0, cast(uint)scheds.length);
+        uint b = uniform!"[)"(0, cast(uint)scheds.length-1);
+        if (a == b) b = cast(uint)scheds.length-1;
+        uint loadA = scheds[a].assigned;
+        uint loadB = scheds[b].assigned;
+        if (loadA < loadB) choice = a;
+        else choice = b;
+    }
+    atomicOp!"+="(scheds[choice].assigned, 1);
     atomicOp!"+="(alive, 1);
+    auto f = new FiberExt(func, choice);
+    logf("Assigned %x -> %d scheduler", cast(void*)f, choice);
+    f.schedule();
 }
 
 package(photon) void schedulerEntry(size_t n)
 {
+    // TODO: handle NUMA case
     wenforce(SetThreadAffinityMask(GetCurrentThread(), 1L<<n), "failed to set affinity");
+    shared SchedulerBlock* sched = scheds.ptr + n;
+    while (alive > 0) {
+        sched.queue.event.waitAndReset();
+        for(;;) {
+            FiberExt f = sched.queue.drain();
+            if (f is null) break; // drained an empty queue, time to sleep
+            do {
+                auto next = f.next; //save next, it will be reused on scheduling
+                currentFiber = f;
+                logf("Fiber %x started", cast(void*)f);
+                try {
+                    f.call();
+                }
+                catch (Exception e) {
+                    stderr.writeln(e);
+                    atomicOp!"-="(alive, 1);
+                }
+                if (f.state == FiberExt.State.TERM) {
+                    logf("Fiber %s terminated", cast(void*)f);
+                    atomicOp!"-="(alive, 1);
+                }
+                f = next;
+            } while(f !is null);
+        }
+    }
+    termination.trigger();
+    foreach (ref s; scheds) {
+        s.queue.event.trigger();
+    }
+}
 
+enum int MAX_COMPLETIONS = 500;
+
+extern(Windows) uint eventLoop(void* param) {
+    HANDLE[2] events;
+    events[0] = iocp;
+    events[1] = cast(HANDLE)termination.ev;
+    for (;;) {
+        auto ret = WaitForMultipleObjects(2, events.ptr, FALSE, INFINITE);
+        if (ret == WAIT_OBJECT_0) { // iocp
+            OVERLAPPED_ENTRY[MAX_COMPLETIONS] entries = void;
+            uint count = 0;
+            while(GetQueuedCompletionStatusEx(iocp, entries.ptr, MAX_COMPLETIONS, &count, 0, FALSE)) {
+                logf("Dequeued I/O events=%d", count);
+                foreach (e; entries[0..count]) {
+                    FiberExt fiber = *cast(FiberExt*)&e.lpCompletionKey;
+                    fiber.bytesTransfered = cast(int)e.dwNumberOfBytesTransferred;
+                    fiber.schedule();
+                }
+                if (count < MAX_COMPLETIONS) break;
+            }
+        }
+        else if (ret == WAIT_OBJECT_0 + 1) { // termination
+            break;
+        }
+        else {
+            logf("Failed to wait for multiple objects: %x", ret);
+            break;
+        }
+    }
+    ExitThread(0);
+    return 0;
+}
+
+
+// ===========================================================================
+// INTERCEPTS
+// ===========================================================================
+
+extern(Windows) SOCKET socket(int af, int type, int protocol) {
+    logf("Intercepted socket!");
+    SOCKET s = WSASocketW(af, type, protocol, null, 0, WSA_FLAG_OVERLAPPED);
+    registerSocket(s);
+    return s;
+}
+
+void registerSocket(SOCKET s) {
+    HANDLE port = iocp;
+    wenforce(CreateIoCompletionPort(cast(void*)s, port, cast(size_t)cast(void*)currentFiber, 0) == port, "failed to register I/O completion");
+}
+
+extern(Windows) int recv(SOCKET s, void* buf, int len, int flags) {
+    OVERLAPPED overlapped;
+    WSABUF wsabuf = WSABUF(cast(uint)len, buf);
+    
+    int ret = WSARecv(s, &wsabuf, 1, null, cast(uint*)&flags, cast(LPWSAOVERLAPPED)&overlapped, null);
+    logf("Got recv %d error: %d", ret, GetLastError());
+    if (ret >= 0) {
+        return ret;
+    }
+    else {
+        FiberExt.yield();
+        return currentFiber.bytesTransfered;
+    }
 }
--- src/photon/windows/support.d
+++ src/photon/windows/support.d
@@ -1,7 +1,63 @@
 module photon.windows.support;
 version(Windows):
 import core.sys.windows.core;
+import core.sys.windows.winsock2;
+import std.format;
 
+struct WSABUF 
+{
+    uint length;
+    void* buf;
+}
+
+extern(Windows) SOCKET WSASocketW(
+  int                af,
+  int                type,
+  int                protocol,
+  void*              lpProtocolInfo,
+  WORD               g,
+  DWORD              dwFlags
+);
+
+extern(Windows) int WSARecv(
+  SOCKET                             s,
+  WSABUF                             *lpBuffers,
+  DWORD                              dwBufferCount,
+  LPDWORD                            lpNumberOfBytesRecvd,
+  LPDWORD                            lpFlags,
+  LPWSAOVERLAPPED                    lpOverlapped,
+  LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine
+);
+
+extern(Windows) int WSASend(
+  SOCKET                             s,
+  WSABUF                             *lpBuffers,
+  DWORD                              dwBufferCount,
+  LPDWORD                            lpNumberOfBytesRecvd,
+  LPDWORD                            lpFlags,
+  LPWSAOVERLAPPED                    lpOverlapped,
+  LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine
+);
+
+struct OVERLAPPED_ENTRY {
+  ULONG_PTR    lpCompletionKey;
+  LPOVERLAPPED lpOverlapped;
+  ULONG_PTR    Internal;
+  DWORD        dwNumberOfBytesTransferred;
+}
+
+alias LPOVERLAPPED_ENTRY = OVERLAPPED_ENTRY*;
+
+extern(Windows) BOOL GetQueuedCompletionStatusEx(
+  HANDLE             CompletionPort,
+  LPOVERLAPPED_ENTRY lpCompletionPortEntries,
+  ULONG              ulCount,
+  PULONG             ulNumEntriesRemoved,
+  DWORD              dwMilliseconds,
+  BOOL               fAlertable
+);
+
+enum WSA_FLAG_OVERLAPPED  =  0x01;
 
 void outputToConsole(const(wchar)[] msg)
 {
@@ -14,6 +70,7 @@ void logf(T...)(const(wchar)[] fmt, T args)
 {
     debug try {
         formattedWrite(&outputToConsole, fmt, args);
+        formattedWrite(&outputToConsole, "\n");
     }
     catch (Exception e) {
         outputToConsole("ARGH!"w);
