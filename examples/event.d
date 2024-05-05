ec8576ccc0121fd47b84cceea0254516958f3375
Dmitry Olshansky <dmitry@olshansky.me>
2024-05-06 00:28:19 +0300

    Cannot implicitly cast event to shared on  MacOS

--- examples/event.d
+++ examples/event.d
@@ -34,8 +34,8 @@ void secondJob() {
 }
 
 void main() {
-    ev1 = event(false);
-    ev2 = event(false);
+    ev1 = cast(shared)event(false);
+    ev2 = cast(shared)event(false);
     startloop();
     go(&firstJob);
     go(&secondJob);
