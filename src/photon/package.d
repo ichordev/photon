00620170290c5000b6c531a7c534397c8e1078e9
Dmitry Olshansky <dmitry@olshansky.me>
2024-05-06 01:10:15 +0300

    Expand on ddocs

--- src/photon/package.d
+++ src/photon/package.d
@@ -1,3 +1,74 @@
+/++
+    Photon is a lightweight transparent fiber scheduler. It's inspired by Golang's green thread model and
+    the spawn function is called `go` doing the same job that Golang's keyword does. 
+    The framework API surface is kept to a minimum, many programs can be written using only 
+    three primitives: `startloop` to initialize Photon, `runFibers` to start fiber scheduler and 
+    `go` to create tasks, including the initial tasks.
+    
+    Example, showcasing channels and std.range interop:
+    ----
+    /+ dub.json:
+        {
+        "authors": [
+            "Dmitry Olshansky"
+        ],
+        "copyright": "Copyright © 2024, Dmitry Olshansky",
+        "dependencies": {
+            "photon": { "path": ".." }
+        },
+        "description": "A test for channels API",
+        "license": "BOOST",
+        "name": "channels"
+    }
+    +/
+    module examples.channels;
+
+    import std.algorithm, std.datetime, std.range, std.stdio;
+    import photon;
+
+    void first(shared Channel!string work, shared Channel!int completion) {
+        delay(2.msecs);
+        work.put("first #1");
+        delay(2.msecs);
+        work.put("first #2");
+        delay(2.msecs);
+        work.put("first #3");
+        completion.put(1);
+    }
+
+    void second(shared Channel!string work, shared Channel!int completion) {
+        delay(3.msecs);
+        work.put("second #1");
+        delay(3.msecs);
+        work.put("second #2");
+        completion.put(2);
+    }
+
+    void main() {
+        startloop();
+        auto jobQueue = channel!string(2);
+        auto finishQueue = channel!int(1);
+        go({
+            first(jobQueue, finishQueue);
+        });
+        go({ // producer # 2
+            second(jobQueue, finishQueue);
+        });
+        go({ // consumer
+            foreach (item; jobQueue) {
+                delay(1.seconds);
+                writeln(item);
+            }
+        });
+        go({ // closer
+            auto completions = finishQueue.take(2).array;
+            assert(completions.length == 2);
+            jobQueue.close(); // all producers are done
+        });
+        runFibers();
+    }
+    ----
++/
 module photon;
 
 import core.thread;
